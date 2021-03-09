03133ef2f7ffc6be00ab5c366aeb94e34e9ef7f6
Anni1123 <annik.ug18.cs@nitp.ac.in>
2021-03-09 23:22:46 +0530

    Initial commit

---
+++ app/src/main/java/com/anni/shareimage/ViewPdfActivity.java
@@ -0,0 +1,58 @@
+package com.anni.shareimage;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
+import android.os.Bundle;
+
+import com.github.barteksc.pdfviewer.PDFView;
+
+import java.io.BufferedInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class ViewPdfActivity extends AppCompatActivity {
+
+    String urls;
+    PDFView pdfView;
+    ProgressDialog dialog;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_pdf);
+        pdfView = findViewById(R.id.abc);
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Loading..");
+        dialog.show();
+        urls = getIntent().getStringExtra("url");
+        new RetrivePdfStream().execute(urls);
+    }
+
+    class RetrivePdfStream extends AsyncTask<String, Void, InputStream> {
+
+        @Override
+        protected InputStream doInBackground(String... strings) {
+            InputStream inputStream = null;
+            try {
+                URL url = new URL(strings[0]);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
+                if (urlConnection.getResponseCode() == 200) {
+                    inputStream = new BufferedInputStream(urlConnection.getInputStream());
+                }
+            } catch (IOException e) {
+                return null;
+            }
+            return inputStream;
+        }
+
+        @Override
+        protected void onPostExecute(InputStream inputStream) {
+            pdfView.fromStream(inputStream).load();
+            dialog.dismiss();
+        }
+    }
+}
